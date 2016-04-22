cff1368453c3032009debb0ea4642d84ec010f5a
Thanya Nitithatsanakul <ammbot@gmail.com>
2016-04-23 06:32:45 +0700

    add GenEvent API

--- lib/event_nanny.ex
+++ lib/event_nanny.ex
@@ -5,14 +5,16 @@ defmodule EventNanny do
 
   import Application, only: [get_env: 3]
 
+  #############
+  # BEHAVIOUR #
+  #############
+
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
-
     children = [
       worker(GenEvent, [[name: event_monitor]]),
       supervisor(@sup, [[event_monitor: event_monitor]])
     ]
-
     opts = [strategy: :one_for_all, name: EventNanny.Supervisor]
     Supervisor.start_link(children, opts)
   end
@@ -24,9 +26,53 @@ defmodule EventNanny do
     :ok
   end
 
+  #######
+  # API #
+  #######
+
   def add_mon_handler(handler, args) do
     Supervisor.start_child @sup, [[handler: handler, args: args]]
   end
+
+  def remove_handler(handler, args) do
+    GenEvent.remove_handler event_monitor, args
+  end
+
+  def swap_mon_handler(handler1, args1, handler2, args2) do
+    GenEvent.swap_mon_handler event_monitor, handler1, args1, handler2, args2
+  end
+
+  def stop(reason \\ :normal, timeout \\ :infinity) do
+    GenEvent.stop event_monitor, reason, timeout
+  end
+
+  def ack_notify(event) do
+    GenEvent.ack_notify event_monitor, event
+  end
+
+  def call(handler, request, timeout \\ 5000) do
+    GenEvent.call event_monitor, handler, request, timeout
+  end
+
+  def stream(timeout \\ :infinity) do
+    GenEvent.stream event_monitor, timeout
+  end
+
+  def sync_notify(event) do
+    GenEvent.sync_notify event_monitor, event
+  end
+
+  def notify(event) do
+    GenEvent.notify event_monitor, event
+  end
+
+  def which_handlers do
+    GenEvent.which_handlers event_monitor
+  end
+
+  ############
+  # INTERNAL #
+  ############
 
   defp event_monitor do
     get_env(:event_nanny, :event_monitor, EventNanny.EventMonitor)
